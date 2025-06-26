7393072ce44813d639a9b708ce37d45224cd7b45
ai-core-tools <ai.core.tools@gmail.com>
2025-06-27 01:26:50 +0300

    MainEcho common function used

--- cmd/echogrpcsrv/main.go
+++ cmd/echogrpcsrv/main.go
@@ -1,79 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	coreControl "github.com/core-tools/hsu-core/go/control"
-	coreDomain "github.com/core-tools/hsu-core/go/domain"
-	coreLogging "github.com/core-tools/hsu-core/go/logging"
 	"github.com/core-tools/hsu-echo-super-srv-go/internal/domain"
-	"github.com/core-tools/hsu-echo-super-srv-go/internal/logging"
 	echoControl "github.com/core-tools/hsu-echo/go/control"
-	echoLogging "github.com/core-tools/hsu-echo/go/logging"
-
-	flags "github.com/jessevdk/go-flags"
 )
 
-type flagOptions struct {
-	Port int `long:"port" description:"port to listen on"`
-}
-
-func logPrefix(module string) string {
-	return fmt.Sprintf("module: %s-server , ", module)
-}
-
 func main() {
-	var opts flagOptions
-	var argv []string = os.Args[1:]
-	var parser = flags.NewParser(&opts, flags.HelpFlag)
-	var err error
-	_, err = parser.ParseArgs(argv)
-	if err != nil {
-		fmt.Printf("Command line flags parsing failed: %v", err)
-		os.Exit(1)
-	}
-
-	logger := logging.NewSprintfLogger()
-
-	logger.Infof("opts: %+v", opts)
-
-	if opts.Port == 0 {
-		fmt.Println("Port is required")
-		os.Exit(1)
-	}
-
-	logger.Infof("Starting...")
-
-	coreLogger := coreLogging.NewLogger(
-		logPrefix("hsu-core"), coreLogging.LogFuncs{
-			Debugf: logger.Debugf,
-			Infof:  logger.Infof,
-			Warnf:  logger.Warnf,
-			Errorf: logger.Errorf,
-		})
-	echoLogger := echoLogging.NewLogger(
-		logPrefix("hsu-echo"), echoLogging.LogFuncs{
-			Debugf: logger.Debugf,
-			Infof:  logger.Infof,
-			Warnf:  logger.Warnf,
-			Errorf: logger.Errorf,
-		})
-
-	coreServerOptions := coreControl.ServerOptions{
-		Port: opts.Port,
-	}
-	coreServer, err := coreControl.NewServer(coreServerOptions, coreLogger)
-	if err != nil {
-		logger.Errorf("Failed to create local server: %v", err)
-		return
-	}
-
-	coreServerHandler := coreDomain.NewDefaultHandler(coreLogger)
-	echoServerHandler := domain.NewSuperHandler(echoLogger)
-
-	echoControl.RegisterGRPCServerHandler(coreServer.GRPC(), echoServerHandler, echoLogger)
-	coreControl.RegisterGRPCServerHandler(coreServer.GRPC(), coreServerHandler, coreLogger)
-
-	coreServer.Run(nil)
+	echoControl.MainEcho(domain.NewSuperHandler)
 }
--- go.mod
+++ go.mod
@@ -2,13 +2,11 @@ module github.com/core-tools/hsu-echo-super-srv-go
 
 go 1.22.3
 
-require (
-	github.com/core-tools/hsu-core v0.0.0-20250619224703-c67bc8d77724
-	github.com/core-tools/hsu-echo v0.0.0-20250619225556-2c5bf95d2356
-	github.com/jessevdk/go-flags v1.6.1
-)
+require github.com/core-tools/hsu-echo v0.0.0-20250626220714-58eca1b61773
 
 require (
+	github.com/core-tools/hsu-core v0.0.0-20250619224703-c67bc8d77724 // indirect
+	github.com/jessevdk/go-flags v1.6.1 // indirect
 	github.com/phayes/freeport v0.0.0-20220201140144-74d24b5ae9f5 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
